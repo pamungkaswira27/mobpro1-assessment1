ba1fbbe42b6d8978402dfa7e9179300769735025
Wira Megantara Pamungkas <89589061+pamungkaswira27@users.noreply.github.com>
2023-03-25 15:10:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/pamungkaswira/kaltika/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pamungkaswira.kaltika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
