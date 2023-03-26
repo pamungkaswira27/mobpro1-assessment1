a2b6bfcb73710c5701fe8e2107910f59ed606bf9
Wira Megantara Pamungkas <89589061+pamungkaswira27@users.noreply.github.com>
2023-03-26 15:42:55 +0700

    Devlog #1 - Build UI Layout

--- app/src/main/java/com/pamungkaswira/kaltika/MainActivity.kt
+++ app/src/main/java/com/pamungkaswira/kaltika/MainActivity.kt
@@ -2,10 +2,15 @@ package com.pamungkaswira.kaltika
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pamungkaswira.kaltika.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
